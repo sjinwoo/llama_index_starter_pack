148b43834d5672030802796f3eb5f4abdbf0fe79
sjinwoo <sjw5953@naver.com>
2023-09-07 18:39:44 +0900

    Split servers

--- flask_react/launch_app.sh
+++ flask_react/launch_app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # start backend index server
-python ./index_server.py &
-echo "index_server running..."
+# python ./index_server.py &
+# echo "index_server running..."
 
 # wait for the server to start - if creating a brand new huge index, on startup, increase this further
-sleep 60
+sleep 30
 
 # start the flask server
 python ./flask_demo.py &
